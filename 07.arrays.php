574bb14d56af09e1b49474d080bf94db3db04998
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-03 20:49:26 -0300

    Arrays

---
+++ 07.arrays.php
@@ -0,0 +1,13 @@
+<?php
+   $array1 = array(40, 200, 100, 150);
+   echo $array1[0];
+   echo "\n";
+   echo $array1[1];
+   echo "\n";
+
+   echo array("Diego", "Cunha", "Maciel")[2];
+   echo "\n";
+
+   $array2 = [40, 200, 100, 150, "School of Net", true];
+   echo $array2[4];
+?>
