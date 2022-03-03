f862bfe1512bee9b9181cf131c354c5b03a1c0a5
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-03 20:24:55 -0300

    Strings

---
+++ 04.strings.php
@@ -0,0 +1,22 @@
+<?php
+    echo "Qualquer coisa";
+    echo "\n";
+
+    $minhaString = "School of Net
+Iniciando com PHP
+Trabalhando com Strings";
+    echo $minhaString;
+    echo "\n";
+
+    $string1 = "School of Net";
+    $string2 = "Iniciando com PHP";    
+    echo $string1 . " - " . $string2;
+    echo "\n";
+
+    $string3 = $string1 . " - " . $string2;
+    echo $string3;
+    echo "\n";
+
+    $string4 = "$string1 - $string2";
+    echo $string4;
+?>
