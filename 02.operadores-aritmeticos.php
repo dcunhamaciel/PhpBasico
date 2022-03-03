a7c3664f4c3a16e72fb3fff62e71b536d7aedf2c
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-03 19:58:12 -0300

    Operadores Aritméticos

---
+++ 02.operadores-aritmeticos.php
@@ -0,0 +1,17 @@
+<?php
+    echo 10 - 4;
+    echo "\n";
+    echo 4 + 3;
+    echo "\n";
+    echo 4 + (3 - 1);
+    echo "\n";
+    echo 5 * 6;
+    echo "\n";
+    echo 5 / 6;
+    echo "\n";
+    echo 4 * 5 / (6 + 3);
+    echo "\n";
+    echo 2 ** 3; // potência
+    echo "\n";
+    echo 5 % 2; // resto da divisão
+?>
