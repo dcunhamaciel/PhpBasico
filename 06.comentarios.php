b9fdca3f44b6552568384555099484f81c7f3803
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-03 20:39:37 -0300

    Comentários

---
+++ 06.comentarios.php
@@ -0,0 +1,20 @@
+<?php
+    // está sendo armazenado a soma de 5 com 3 na variável $total
+    $total = 5 + 3;
+
+    /* 
+       School of Net
+       Curso: Iniciando com PHP
+    */
+
+    // comentário de uma linha
+    /* comentário multilinha */
+
+    /************************** 
+     * 
+     * Texto
+     * Aqui
+     * 
+     * 
+     ***************************/
+?>
