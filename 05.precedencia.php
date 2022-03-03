4fb3d2b6c122aa4c6a230dad0bcf453e6ccdae39
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-03 20:32:17 -0300

    Precedência

---
+++ 05.precedencia.php
@@ -0,0 +1,17 @@
+<?php
+    $total = 5 + 3 * 8;
+    $total = 5 + 24;
+    $total = 29;
+    echo 5 + 3 * 8;
+    echo "\n";
+    echo $total;
+    echo "\n";
+
+    $total = (5 + 3 * 5) * 8;
+    $total = (5 + 15) * 8;
+    $total = (20) * 8;
+    $total = 160;
+    echo (5 + 3 * 5) * 8;
+    echo "\n";
+    echo $total;
+?>
