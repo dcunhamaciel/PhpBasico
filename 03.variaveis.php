c12e77a8076a100e13780984ed497180cffb51a5
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-03 20:09:14 -0300

    Variáveis

---
+++ 03.variaveis.php
@@ -0,0 +1,28 @@
+<?php
+    $nome = "School of Net";
+    echo $nome;
+    echo "\n";
+
+    $nome = "Iniciando com PHP";
+    echo $nome;
+    echo "\n";
+
+    $nome = 5000; // tipagem fraca
+    echo $nome;
+    echo "\n";
+
+    $numero = 5000;
+    echo $numero;  
+    echo "\n";
+
+    $nome = "Iniciando com PHP";
+    $Nome = 'School Of Net'; // case sensitive
+    echo $nome . " - " . $Nome;
+    echo "\n";
+
+    $variavelTrue = true;
+    $variavelFalse = false;
+    echo $variavelTrue; // imprime 1
+    echo "\n";
+    echo $variavelFalse; // imprime nada
+?>
