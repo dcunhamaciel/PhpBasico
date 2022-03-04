76b2f0c7c6a2eb99b9c61426f8d37fdd9b76d386
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-04 08:48:03 -0300

    Arrays Chave/Valor

---
+++ 08.arrays-chave-valor.php
@@ -0,0 +1,16 @@
+<?php
+   $array = array(
+       'chave1' => 40, 
+       'chave2' => 200,
+       100, 
+       150, 
+       "School of Net", 
+       true
+   );
+
+   echo $array['chave1'];
+   echo "\n";
+   echo $array[1];
+   echo "\n";
+   var_dump($array);
+?>
